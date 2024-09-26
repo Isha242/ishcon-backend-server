eb859fda05bcb4182e6041187a06bbd14af7df61
Feecon <mail2feeconbehera@gmail.com>
2024-09-26 07:22:27 +0530

    origin change

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const DB_URI = process.env.DB_URI;
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "https://ishcon-frontend.vercel.app",
+    origin: "https://ishcon.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Custom headers if needed
   })
