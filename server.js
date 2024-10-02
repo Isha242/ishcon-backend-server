5c14de596b5fe05c9d3c4441eeebf858af0ff049
Feecon <mail2feeconbehera@gmail.com>
2024-10-03 02:43:27 +0530

    env chnages

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-require("dotenv").config();
+require("dotenv").config(); // Load env variables
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors");
@@ -9,22 +9,26 @@ const userRouter = require("./routers/userRouter");
 
 app.use(express.json());
 
-
 const PORT = process.env.PORT || 5001;
 const DB_URI = process.env.DB_URI;
+const CLIENT_URL_PROD = process.env.CLIENT_URL_PROD; // Get client URL from env
+const CLIENT_URL_STAGE = process.env.CLIENT_URL_STAGE; // Get client URL from env
+const CLIENT_URL_DEV = process.env.CLIENT_URL_DEV; // Get client URL from env
+
+console.log(`DB_URI: ${DB_URI}`);
 
 // Middleware
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "https://ishcon.vercel.app", // Allow to server to accept request from different origin
+    origin: [CLIENT_URL_PROD, CLIENT_URL_STAGE, CLIENT_URL_DEV], // Dynamically allow requests from the correct client
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed methods
     allowedHeaders: ["Content-Type", "Authorization"], // Custom headers if needed
   })
 );
 
-/** Database connection starts */
-mongoose
+// Database connection
+mongoose 
   .connect(DB_URI)
   .then(() => {
     console.log("Connection to MongoDB is established!");
@@ -33,11 +37,11 @@ mongoose
     console.log("Something went wrong with DB connection", err);
   });
 
-/** Routers */
+// Routers
 app.use("/api/messages", messageRouter);
 app.use("/api/users", userRouter);
 
-// Fallback middleware for 404 errors
+// Fallback for 404 errors
 app.use(function (req, res) {
   res.status(404).send("404 Not Found!");
 });
@@ -46,4 +50,3 @@ app.use(function (req, res) {
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
-
